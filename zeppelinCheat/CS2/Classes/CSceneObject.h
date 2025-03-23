2e2359c56111f5e69b1dcad75f199584d2cd6176
Fujiwara <83638372+AZRAELSANTI@users.noreply.github.com>
2025-03-24 01:25:48 +0300

    1

--- zeppelinCheat/CS2/Classes/CSceneObject.h
+++ zeppelinCheat/CS2/Classes/CSceneObject.h
@@ -5,25 +5,25 @@
 namespace cs2
 {
 
-enum SceneObjectFlags {
-    SceneObjectFlag_IsDeleted = 0x20
-};
+    enum SceneObjectFlags {
+        SceneObjectFlag_IsDeleted = 0x20
+    };
 
-enum ESceneObjectFlags : std::uint64_t {
-    SCENEOBJECTFLAG_PIPELINE_SPECIFIC_2 = (std::uint64_t{1} << 54)
-};
+    enum ESceneObjectFlags : std::uint64_t {
+        SCENEOBJECTFLAG_PIPELINE_SPECIFIC_2 = (std::uint64_t{ 1 } << 54)
+    };
 
-struct SceneObjectAttributes {
-    struct FloatAttributes {};
+    struct SceneObjectAttributes {
+        struct FloatAttributes {};
 
-    FloatAttributes floatAttributes;
-};
+        FloatAttributes floatAttributes;
+    };
 
-struct CSceneObject {
-    using m_nObjectClass = std::uint8_t;
-    using flags = std::uint8_t;
-    using attributes = SceneObjectAttributes*;
-    using m_nRenderableFlags = std::uint64_t;
-};
+    struct CSceneObject {
+        using m_nObjectClass = std::uint8_t;
+        using flags = std::uint8_t;
+        using attributes = SceneObjectAttributes*;
+        using m_nRenderableFlags = std::uint64_t;
+    };
 
 }
